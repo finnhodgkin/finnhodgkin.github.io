5c098426c55ee5f1b1bce9a795f41ad1933cadc1
finnhodgkin <finnhodgkin@gmail.com>
2016-11-01 18:30:56 +0000

    Simplifying

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
 $(document).ready(function(){
+	var b = function(event) {
+		event.preventDefault();
+		$(this).hasClass("i1") ? $(".ti").fadeIn(300).addClass('active').addClass('b').removeClass('a'):
+		$(".ti").fadeIn(300).addClass('active').addClass('a').removeClass('b')
+	}
+	$('.i1').click(b);$('.i2').click(b);
+	$('.ti').on('click', function(){$(".ti").fadeOut(300).removeClass('active')})
+
 	var a = "", c = "";
-
-	$('.image1').click(function(event) {
-		event.preventDefault();
-		$(".timage").fadeIn(300).addClass('active-slide').addClass('timagec');
-  });
-	$('.image2').click(function(event) {
-		event.preventDefault();
-		$(".timage").fadeIn(300).addClass('active-slide').removeClass('timageb').removeClass('timagec').addClass('timagea');
-  });
-	$('.image3').click(function(event) {
-		event.preventDefault();
-		$(".timage").fadeIn(300).addClass('active-slide').removeClass('timagec').addClass('timageb');
-  });
-	$('.timage').on('click', function(){
-		$(".timage").fadeOut(300).removeClass('active-slide');
-  });
-
 	$('#wrapper a').bind('click', function(e){
 		c = $(this).attr("href");
-		if($('#hiddenMobileTest').css('float') === 'left'){
+		if($('#hiddenMobileTest').css('float') === 'left')
 			if(a !== c){
 				e.preventDefault();
 				a = $(this).attr("href");
 			}
-		}
-		});
 	});
+});
 
+$(window).on('load resize', function () {
+  var bH = $('.box').width(),
+      tH = $('.tinyBox').width(),
+      m = ($('#wrapper').css('margin-left').replace("px", "") / 4) + "px",
+      boxM = $('.box').css('margin-left').replace("px", "") * 1;
 
-
-  $(window).on('load resize', function () {
-    var bH = $('.box').width(),
-        tH = $('.tinyBox').width(),
-        m = ($('#wrapper').css('margin-left').replace("px", "") / 2) + "px",
-        boxM = $('.box').css('margin-left').replace("px", "") * 1;
-
-  	$('#wrapper').css('marginTop', m).css('marginBottom', m);
-    $('.box').css('height', bH);
-  	$('.bigBox').css('height', bH);
-    $('.tinyBox').css('height', tH);
-  	$('.timage').css('margin', boxM + "px")
-                .height((bH + boxM) *2)
-                .width((bH + boxM) *2);
+	$('#wrapper').css('marginTop', m).css('marginBottom', m);
+  $('.box').css('height', bH);
+	$('.bigBox').css('height', bH);
+  $('.tinyBox').css('height', tH);
+	$('.ti').css('margin', boxM + "px")
+          .height((bH + boxM) *2)
+          .width((bH + boxM) *2);
 });
