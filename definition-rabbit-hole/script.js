74216f80eebb06920ac61d3b2cb617f751a85704
finnhodgkin <finnhodgkin@gmail.com>
2016-11-24 20:46:10 +0000

    Completed basic functionality

--- definition-rabbit-hole/script.js
+++ definition-rabbit-hole/script.js
@@ -1,24 +1,39 @@
 window.onload = function() {
-
     var w = document.getElementById("word"),
-    word = w.value;
-
+    word = w.value,
+    words;
+    w.value = "";
     w.addEventListener("keyup", function(){
       word = w.value.toLowerCase();
       if(word.indexOf(" ") >= 0){
         word = word.split(" ");
-        $.getJSON('https://api.wordnik.com:80/v4/word.json/' + word[0] + '/definitions?limit=200&includeRelated=false&sourceDictionaries=wiktionary&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').done(function(definition){
-          document.getElementById('results').innerHTML =
-          "<h2>" + definition[0].word + "</h2>" + "<i><b>" + definition[0].partOfSpeech + "</b></i><br><br>" + split(definition[0].text);
-          document.getElementById('results').className = "on";
-        });
+        wordnik(word[0]);
       }
     });
 };
+function wordnik (wordN){
+  $.getJSON('http://api.wordnik.com:80/v4/word.json/' + wordN + '/definitions?limit=1&includeRelated=false&sourceDictionaries=wiktionary&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').done(function(definition){
+    if(definition[0]){
+      document.getElementById('results').innerHTML =
+      "<h2>" + definition[0].word + "</h2>" + "<i><b>" +    definition[0].partOfSpeech + "</b></i><br><br>" + split(definition[0].text);
+      document.getElementById('results').className = "on";
+      words = document.getElementsByTagName('span');
+      for(i = 0; i < words.length; i++){
+        words[i].addEventListener('click', function(e){
+          wordnik(this.innerHTML.toLowerCase().replace(/[^a-z]/g,""))
+        });
+      }
+      document.getElementById("word").className = "hide"
+    }
+    else{
+      document.getElementById('results').innerHTML = "Sorry, couldn't find a definition for that word. Try again.";
+      document.getElementById('results').className = "on";
+      document.getElementById("word").className = "show";
+      document.getElementById("word").value = "";
+    }
 
-
-
-
+  });
+}
 function split (text) {
   text = text.split(" ");
   text[0] = "<span>" + text[0];
