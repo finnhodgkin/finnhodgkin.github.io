8ec14c4ef6b95aaf7dcd4db36b64fb99afe18fac
finnhodgkin <finnhodgkin@gmail.com>
2016-11-24 19:14:49 +0000

    enable dictionary search and display with spacebar

--- definition-rabbit-hole/script.js
+++ definition-rabbit-hole/script.js
@@ -1,9 +1,23 @@
 window.onload = function() {
 
-    var test = "The best meat is served cold: sashimi, steak, oysters, carpacio."
+    var w = document.getElementById("word"),
+    word = w.value;
 
-    //document.getElementById('results').innerHTML = split(test);
+    w.addEventListener("keyup", function(){
+      word = w.value.toLowerCase();
+      if(word.indexOf(" ") >= 0){
+        word = word.split(" ");
+        $.getJSON('http://api.wordnik.com:80/v4/word.json/' + word[0] + '/definitions?limit=200&includeRelated=false&sourceDictionaries=wiktionary&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').done(function(definition){
+          document.getElementById('results').innerHTML =
+          "<h2>" + definition[0].word + "</h2>" + "<i><b>" + definition[0].partOfSpeech + "</b></i><br><br>" + split(definition[0].text);
+          document.getElementById('results').className = "on";
+        });
+      }
+    });
 };
+
+
+
 
 function split (text) {
   text = text.split(" ");
