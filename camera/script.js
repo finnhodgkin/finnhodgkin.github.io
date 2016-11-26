d102c7c3fc2145817ad6e3cf8ae181ee444919fe
Finn Hodgkin <finnhodgkin@gmail.com>
2016-11-26 16:21:42 +0000

    add polaroid it

--- camera/script.js
+++ camera/script.js
@@ -1,1 +1,21 @@
-document.getElementById('picture').className = "anim";
+window.onload = function() {
+  setTimeout(function () {
+    document.getElementById('picture').className = "anim";
+  }, 500);
+
+  document.getElementById('imageFile').addEventListener('change', readURL, true);
+}
+
+
+
+function readURL(){
+   var file = document.getElementById("imageFile").files[0];
+   var reader = new FileReader();
+   reader.onloadend = function(){
+      document.getElementById('picture').style.backgroundImage = "url(" + reader.result + ")";
+   }
+   if(file){
+      reader.readAsDataURL(file);
+    }else{
+    }
+}
