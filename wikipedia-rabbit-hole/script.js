6c4760438457dc938f7d932656e71a00fa47f1ca
Finn Hodgkin <finnhodgkin@gmail.com>
2016-11-26 13:00:49 +0000

    add error messaging

--- wikipedia-rabbit-hole/script.js
+++ wikipedia-rabbit-hole/script.js
@@ -7,14 +7,14 @@ window.onload = function() {
         let event = e || window.event,
             charCode = event.which || event.keyCode;
         if ( charCode == '13' ){
+          document.getElementById('T').innerHTML = "";
           wikipedia(w.value);
-          w.value = "";
         }
     }
 };
 
 function wikipedia (value){
-  let words = "";
+  let words = "", res = document.getElementById('results');
   $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + value + '&namespace=0&origin=*&limit=5').done(function(wiki) {
     if(wiki[2][0]){
       words = split(wiki[2][0]) + "<br><br>";
@@ -23,13 +23,12 @@ function wikipedia (value){
            if(wiki[2][i])words += split(wiki[2][i]) + " <a href='" + wiki[3][i] + "'>(link)</a>" + "<br><br>";
         }
       }
-      else {
-        words = words.slice(0,-8) + " <a href='" + wiki[3][0] + "'>(link)</a><br><br>";
-      }
+      else words = words.slice(0,-8) + " <a href='" + wiki[3][0] + "'>(link)</a><br><br>";
     }
-    else words = "Sorry, there were no matching results for \"" + value + "\"." + "<br><br>";
+    else if(!res.innerHTML.includes("Sorry, there were no matching results"))words += res.innerHTML + "<br><br><div class='red'>Sorry, there were no matching results for \"" + value + "\".</div>" + "<br><br>";
+    else words = res.innerHTML + "<br><br>";
 
-    document.getElementById('results').innerHTML = words.slice(0,-8);
+    res.innerHTML = words.slice(0,-8);
     let spans = document.getElementsByTagName('span');
     for(i = 0; i < spans.length; i++){
       spans[i].addEventListener('click', addSpans);
@@ -37,8 +36,10 @@ function wikipedia (value){
    });
 }
 function addSpans(){
-    removeEvents(document.getElementsByTagName('span'));
-    wikipedia(this.innerHTML.toLowerCase().replace(/[^a-z]/g,""));
+  if(this.innerHTML.toLowerCase().replace(/[^a-z0-9]/g,"")){
+      removeEvents(document.getElementsByTagName('span'));
+      wikipedia(this.innerHTML.toLowerCase().replace(/[^a-z0-9]/g,""));
+    }
 }
 function removeEvents(item){
   for(a = 0; a < item.length; a++)
